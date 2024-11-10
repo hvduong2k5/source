fd02a44343d757b642f7749e3dab875dca1806e3
HvDuong <hd2005555@gmail.com>
2024-11-10 10:15:30 +0700

    edit background

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: #FFDDC1;");  // Màu cam nhạt
+
 }
 
 MainWindow::~MainWindow()
