4df579681fd42d1d613bd6c7c1714d0e7fc0f767
HvDuong <hd2005555@gmail.com>
2024-11-10 10:20:18 +0700

    edit and include login

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include "loginwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,5 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    LoginWindow login;
+    login.show();
     return a.exec();
 }
